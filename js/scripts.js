3ff6496d5faa5266dd45dcfd3ae1616dfa4642e5
Garland Lai <garlandlai24@gmail.com>
2019-06-21 08:42:10 -0700

    add objects and prototypes to calculatecost of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
 
-$(document).ready(function(){
 
+function YourCart() {
+  this.pizza = [];
+}
 
-});
+function Pizza(size,toppings) {
+  this.size = size;
+  this.toppings = topping;
+}
+
+YourCart.prototype.addPizza = function(pizza) {
+  this.pizza.push(Pizza);
+}
+
+YourCart.prototype.sizeCost = function(size) {
+  if (this.size === large) {
+    this.size = 18
+  } else if (this.size === medium) {
+    this.size = 14
+  } else {
+    this.size = 10
+  }
+}
+
+YourCart.prototype.toppingsCost = function(toppings) {
+  if (this.toppings === 5) {
+    this.toppings = 5
+  } else if (this.toppings === 4) {
+    this.toppings = 4
+  } else if (this.toppings === 4) {
+    this.toppings = 3
+  } else if (this.toppings === 2) {
+    this.toppings = 2
+  } else if (this.toppings === 1){
+    this.toppings = 1
+  } else {
+    this.toppings = 0
+  }
+}
