6f99321341f800e7c321cb4b0f7106a81b3efd34
Garland Lai <garlandlai24@gmail.com>
2019-06-21 12:34:12 -0700

    calculate the pizza cost in a function

--- js/scripts.js
+++ js/scripts.js
@@ -13,28 +13,40 @@ function Pizza(size,toppings) {
 YourCart.prototype.addPizza = function(Pizza) {
   this.pizza.push(Pizza);
 }
-//displays toppings picked
 
-Pizza.prototype.Calctoppings = function() {
-  if (this.toppings === 5) {
-    this.toppings = 5
-  } else if (this.toppings === 4) {
-    this.toppings = 4
-  } else if (this.toppings === 3) {
-    this.toppings = 3
-  } else if (this.toppings === 2) {
-    this.toppings = 2
-  } else if (this.toppings === 1){
-    this.toppings = 1
+//calculate cost of pizza
+
+Pizza.prototype.pizzaCost = function() {
+  if (this.size === "Large") {
+    console.log(18 + this.toppings.length)
+  } else if (this.size === "Medium"){
+    console.log(14 + this.toppings.length)
   } else {
-    this.toppings = 0
+    console.log(10 + this.toppings.length)
   }
 }
+//
+//   }
+//
+//   if (this.toppings === 5) {
+//     this.toppings = 5
+//   } else if (this.toppings === 4) {
+//     this.toppings = 4
+//   } else if (this.toppings === 3) {
+//     this.toppings = 3
+//   } else if (this.toppings === 2) {
+//     this.toppings = 2
+//   } else if (this.toppings === 1){
+//     this.toppings = 1
+//   } else {
+//     this.toppings = 0
+//   }
+// }
 
+//
+// YourCart.prototype.totalCost = function() {
+//     this.toppings += this.size
 
-YourCart.prototype.totalCost = function() {
-    this.toppings += this.size
-}
 
 var currentCart = new YourCart();
 $(document).ready(function(){
@@ -55,9 +67,11 @@ $(document).ready(function(){
   currentCart.addPizza(newPizza);
   // console.log(currentCart);
 
-  var pizzaPrice = currentCart.totalCost();
-  // console.log(pizzaPrice);
+  var totalCost = currentCart.pizza[0].pizzaCost();
+  // var pizzaPrice = currentCart.totalCost();
+  // // console.log(pizzaPrice);
 
-  $(".output").text("$ " + pizzaPrice + ".00");
+  // $(".output").text("$ " + pizzaPrice + ".00");
+  console.log(currentCart.pizza[0].toppings.length);
   });
 });
