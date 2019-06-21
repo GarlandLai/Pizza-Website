658da2d90acb14b9a200de05835c6725ddd1d80f
Garland Lai <garlandlai24@gmail.com>
2019-06-21 12:06:48 -0700

    change from value from numbers to strings and sucessfully push the new object to the cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,21 @@
 
-
+//stores pizzas
 function YourCart() {
   this.pizza = [];
 }
 
+//pizza constructor
 function Pizza(size,toppings) {
   this.size = size;
-  this.toppings = 0;
+  this.toppings = toppings;
 }
-
-YourCart.prototype.addPizza = function(pizza) {
+//pushed the current pizze to cart
+YourCart.prototype.addPizza = function(Pizza) {
   this.pizza.push(Pizza);
 }
+//displays toppings picked
 
-YourCart.prototype.sizeCost = function(size) {
-  if (this.size === 18) {
-    this.size = 18
-  } else if (this.size === 14) {
-    this.size = 14
-  } else {
-    this.size = 10
-  }
-}
-
-YourCart.prototype.toppingsCost = function(toppings) {
+Pizza.prototype.Calctoppings = function() {
   if (this.toppings === 5) {
     this.toppings = 5
   } else if (this.toppings === 4) {
@@ -45,26 +37,26 @@ YourCart.prototype.totalCost = function() {
 }
 
 var currentCart = new YourCart();
-
 $(document).ready(function(){
   $("#pizzaMenu").submit(function(){
   event.preventDefault();
-  var sizeInput = parseInt($("#pizzaSize").val());
-  var size = currentCart.sizeCost(sizeInput);
-  var totalToppings = []
+  //displays size picked
+  var sizeInput = $("#pizzaSize").val();
+  //displays toppings picked
+  var totalPicked = []
   var toppingInput =
-  $("input:checkbox[name=numToppings]:checked").each(function(){
-    var checked = parseInt($(this).val());
-    totalToppings.push(checked);
-  });
-  console.log(totalToppings);
+    $("input:checkbox[name=numToppings]:checked").each(function(){
+      var checked = $(this).val();
+      totalPicked.push(checked);
+    });
+    console.log(totalPicked);
 
-
-  currentCart.addPizza();
-  console.log(currentCart);
+  var newPizza = new Pizza(sizeInput,totalPicked);
+  currentCart.addPizza(newPizza);
+  // console.log(currentCart);
 
   var pizzaPrice = currentCart.totalCost();
-  console.log(pizzaPrice);
+  // console.log(pizzaPrice);
 
   $(".output").text("$ " + pizzaPrice + ".00");
   });
